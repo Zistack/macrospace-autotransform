b210fbc2bc1e1f54bc2ef498d342ca4cd121658b
Zistack <Zistack@null.net>
2025-07-09 15:50:01 -0400

    Added AutotransformBank type for transforming types with a whole set of autotransformations simultaneously

---
+++ src/autotransform_bank.rs
@@ -0,0 +1,56 @@
+use proc_macro2::TokenStream;
+
+use crate::{Autotransform, TransformDirection, Forward, Backward, ApplicationError};
+
+#[derive (Clone, Debug)]
+pub struct AutotransformBank
+{
+	transforms: Vec <Autotransform>
+}
+
+impl AutotransformBank
+{
+	pub fn try_apply <D> (&self, ty_tokens: TokenStream)
+	-> Result <(TokenStream, TokenStream), ApplicationError>
+	where D: TransformDirection
+	{
+		for transform in &self . transforms
+		{
+			match transform . try_apply::<D, _>
+			(
+				ty_tokens . clone (),
+				|ty_tokens| self . try_apply::<D> (ty_tokens)
+			)
+			{
+				Ok ((transformed_ty, transformed_closure)) =>
+					return Ok ((transformed_ty, transformed_closure)),
+				e @ Err (ApplicationError::Substitute (_)) => return e,
+				_ => continue
+			}
+		}
+
+		Err
+		(
+			ApplicationError::Match
+			(
+				syn::parse::Error::new_spanned
+				(
+					ty_tokens,
+					"type does not match any autotransform pattern"
+				)
+			)
+		)
+	}
+
+	pub fn try_apply_forward (&self, ty_tokens: TokenStream)
+	-> Result <(TokenStream, TokenStream), ApplicationError>
+	{
+		self . try_apply::<Forward> (ty_tokens)
+	}
+
+	pub fn try_apply_backward (&self, ty_tokens: TokenStream)
+	-> Result <(TokenStream, TokenStream), ApplicationError>
+	{
+		self . try_apply::<Backward> (ty_tokens)
+	}
+}
